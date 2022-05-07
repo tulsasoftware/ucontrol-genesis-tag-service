2501e861b75a95ad4c5401e3b77fba6c37a0fd4a
Corey Thompson <corey.dwayne.thompson@gmail.com>
2022-05-07 08:23:27 -0500

    attempt first build

---
+++ .github/dev/multi-arch-build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Local .env
+echo ".env will be loaded if it exists...";
+if [ -f .env ]; then
+    # Load Environment Variables
+    export $(cat .env | grep -v '#' | sed 's/\r$//' | awk '/=/ {print $1}' )
+    echo "Loaded .env file";
+fi
+
+# Check if a local builder already exists
+echo "Checking if a docker builder already exists...";
+if [ $(docker buildx ls  2>/dev/null | wc -l) > 0 ]; then
+    echo "Creating Docker builder...";
+    docker buildx create --name "ucontrol_builder" --use
+    echo "Creating Docker builder... Done."
+fi
+
+echo "Logging into container registry as ${GITHUB_USER}..."
+docker login ghcr.io -u ${GITHUB_USER} -p ${GITHUB_PAT} 
+echo "Logging into container registry... Done."
+
+echo "Starting Docker image build..."
+echo "Tagging image with '${TAG_SERVICE_TAG}'..."
+docker buildx build --push --platform linux/amd64,linux/arm64,linux/arm/v7,linux/arm/v6 --tag ghcr.io/${TAG_SERVICE_PROJECT}:${TAG_SERVICE_TAG} .
+echo "Docker image build... Done."
